f326cda3774b4f52a8e958495e7b80f21f3bc39f
Marien Mupenda <marien.mupenda@gmail.com>
2026-08-30 22:16:57 +0200

    add receipt

--- routes/api.php
+++ routes/api.php
@@ -6,10 +6,10 @@ use App\Http\Controllers\Api\FiliereController;
 use App\Http\Controllers\Api\InscriptionController;
 use App\Http\Controllers\Api\OptionController;
 use App\Http\Controllers\Api\SectionController;
-use App\Http\Controllers\Api\V1\Finance\ReceiptController;
 use App\Http\Controllers\Api\V1\AuthController;
 use App\Http\Controllers\Api\V1\ContextController;
 use App\Http\Controllers\Api\V1\DashboardController;
+use App\Http\Controllers\Api\V1\ReceiptController;
 use App\Http\Resources\AnneeResource;
 use App\Models\Annee;
 use Illuminate\Support\Facades\Route;
@@ -27,10 +27,7 @@ Route::prefix('v1')->group(function () {
         Route::get('/dashboard', [DashboardController::class, 'index']);
     });
 
-    Route::prefix('finance')->group(function () {
 
-        Route::get( 'receipts', [ReceiptController::class, 'index'])->name('api.v1.finance.receipts.index');
-
-    });
+    Route::get('/receipts', [ReceiptController::class, 'index']);
 
 });
